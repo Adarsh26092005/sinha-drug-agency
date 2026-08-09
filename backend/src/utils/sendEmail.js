2ac89f987eb68320884018a23d6a83de36fd6ed6
Adarsh <kumaradarsh26092005@gmail.com>
2026-08-09 21:42:38 +0530

    switch to resend

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,16 +1,10 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_APP_PASSWORD,
-    },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ to, subject, html }) => {
-    await transporter.sendMail({
-        from: `"Sinha Drug Agency" <${process.env.EMAIL_USER}>`,
+    await resend.emails.send({
+        from: "Sinha Drug Agency <onboarding@resend.dev>",
         to,
         subject,
         html,
